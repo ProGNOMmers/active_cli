5db86e4e10f0e17959c06e64c4c4a9d18b225df1
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-11-15 00:31:24 +0100

    ActiveCLI::CLI.new: arguments setting

--- lib/active_cli/cli.rb
+++ lib/active_cli/cli.rb
@@ -1,7 +1,12 @@
 module ActiveCLI
   class CLI
+
+    attr_reader :arguments
+
     def initialize(arguments = [])
       raise ArgumentError, "arguments is not an Array" unless arguments.is_a? Array
+
+      @arguments = arguments
     end
   end
 end
--- spec/active_cli/cli_spec.rb
+++ spec/active_cli/cli_spec.rb
@@ -4,24 +4,30 @@ module ActiveCLI
   describe CLI do
     describe '.new' do
       context 'with no arguments' do
-        let(:arguments) { [] }
         it 'works' do
+          arguments = []
           expect { described_class.new *arguments }.to_not raise_error
         end
       end
       context 'with an array argument' do
-        let(:arguments) { [ [] ] }
         it 'works' do
+          arguments = [ [] ]
           expect { described_class.new *arguments }.to_not raise_error
         end
       end
       context 'with a non-array argument' do
-        let(:arguments_list) { [ nil, false, true, 1, Object.new, {} ] }
         it 'raises an ArgumentError' do
-          arguments_list.each do |argument|
+          arguments = [ nil, false, true, 1, Object.new, '', {} ]
+          arguments.each do |argument|
             expect { described_class.new *[ argument ] }.to raise_error ArgumentError
           end
         end
+      end
+      it 'sets arguments instance variable' do
+        arguments = [ '--help' ]
+        cli = described_class.new arguments
+        expect( cli.instance_variable_get :@arguments ).to be arguments
+        expect( cli.arguments                         ).to be arguments
       end
     end
   end
