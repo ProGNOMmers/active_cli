3c5e5b328d749740334cd1f93bf60ff352074cbc
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-11-15 00:32:37 +0100

    Refactoring

--- spec/active_cli/cli_spec.rb
+++ spec/active_cli/cli_spec.rb
@@ -6,26 +6,30 @@ module ActiveCLI
       context 'with no arguments' do
         it 'works' do
           arguments = []
+
           expect { described_class.new *arguments }.to_not raise_error
         end
       end
       context 'with an array argument' do
         it 'works' do
           arguments = [ [] ]
+
           expect { described_class.new *arguments }.to_not raise_error
         end
       end
       context 'with a non-array argument' do
         it 'raises an ArgumentError' do
           arguments = [ nil, false, true, 1, Object.new, '', {} ]
+
           arguments.each do |argument|
             expect { described_class.new *[ argument ] }.to raise_error ArgumentError
           end
         end
       end
       it 'sets arguments instance variable' do
-        arguments = [ '--help' ]
-        cli = described_class.new arguments
+        arguments = [ ]
+        cli       = described_class.new arguments
+        
         expect( cli.instance_variable_get :@arguments ).to be arguments
         expect( cli.arguments                         ).to be arguments
       end
