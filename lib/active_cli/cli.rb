b4513dcc0a8bdd25e87088b38cc473be41983351
Maurizio De Santis <m.desantis@morganspa.com>
2013-11-15 12:10:24 +0100

    ActiveCLI::CLI.new: yield self, remove arguments check

--- lib/active_cli/cli.rb
+++ lib/active_cli/cli.rb
@@ -3,10 +3,12 @@ module ActiveCLI
 
     attr_reader :arguments
 
-    def initialize(arguments = [])
-      raise ArgumentError, "arguments is not an Array" unless arguments.is_a? Array
+    def initialize(arguments = ARGV)
+      # raise ArgumentError, "arguments is not an Array" unless arguments.is_a? Array
 
       @arguments = arguments
+
+      yield self if block_given?
     end
   end
 end
--- spec/active_cli/cli_spec.rb
+++ spec/active_cli/cli_spec.rb
@@ -8,33 +8,45 @@ module ActiveCLI
         example.metadata[:arguments]
       end
 
-      context 'with no arguments', arguments: [ ] do
-        it 'works' do |example|
-          expect { described_class.new *arguments(example) }.to_not raise_error
-        end
-      end
+      # context 'with invalid arguments' do
+      #   it 'raises an ArgumentError', arguments_list: [ nil, false, true, 1, Object.new, '', {} ] do |example|
+      #     example.metadata[:arguments_list].each do |argument|
+      #       expect { described_class.new *[ argument ] }.to raise_error ArgumentError
+      #     end
+      #   end
+      # end
 
-      context 'with an array argument', arguments: [ [] ] do
-        it 'works' do |example|
-          expect { described_class.new *arguments(example) }.to_not raise_error
-        end
-      end
-
-      context 'with a non-array argument' do
-        it 'raises an ArgumentError', arguments_list: [ nil, false, true, 1, Object.new, '', {} ] do |example|
-          example.metadata[:arguments_list].each do |argument|
-            expect { described_class.new *[ argument ] }.to raise_error ArgumentError
+      # context 'with valid arguments' do
+        context 'with no arguments', arguments: [ ] do
+          it 'works' do |example|
+            expect { described_class.new *arguments(example) }.to_not raise_error
+          end
+          it 'sets .arguments to ARGV' do |example|
+            expect( described_class.new( *arguments(example) ).arguments ).to be ARGV
           end
         end
-      end
 
-      it 'sets arguments instance variable', arguments: [ ] do |example|
-        arguments = arguments(example)
-        cli       = described_class.new arguments
+        context 'with an array argument', arguments: [ [] ] do
+          it 'works' do |example|
+            expect { described_class.new *arguments(example) }.to_not raise_error
+          end
+        end
 
-        expect( cli.instance_variable_get :@arguments ).to be arguments
-        expect( cli.arguments                         ).to be arguments
-      end
+        context 'giving a block with one argument', arguments: [ ] do
+          it 'yields self' do |example|
+            # TODO it doesn't check for `self` identity; I don't know how to implement it
+            expect { |b| described_class.new *arguments(example), &b }.to yield_with_args described_class
+          end
+        end
+
+        it 'sets arguments instance variable', arguments: [ ] do |example|
+          arguments = arguments(example)
+          instance  = described_class.new arguments
+
+          expect( instance.instance_variable_get :@arguments ).to be arguments
+          expect( instance.arguments                         ).to be arguments
+        end
+      # end
 
     end
   end
